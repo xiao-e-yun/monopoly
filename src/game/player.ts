e763a1b967263d045d302527885986bbd3e64444
xiao-e-yun <a0970323059@gmail.com>
2024-12-02 00:15:58 +0800

    修改生命值

--- src/game/player.ts
+++ src/game/player.ts
@@ -7,7 +7,7 @@ import { useGameRender } from "./render";
 import { useGameState } from "./state";
 import { shuffle } from "./utils";
 
-export const DEFAULT_PLAYER_HEALTH = 5
+export const DEFAULT_PLAYER_HEALTH = 6
 export class Player {
 
   active = false
@@ -35,7 +35,7 @@ export class Player {
     if (!this.fixedDirection) {
       this.direction = accepts.size === 1 ?
         accepts.keys().next().value! :
-        await inputs.move.input("選擇方向",Array.from(accepts.keys()))
+        await inputs.move.input("選擇方向", Array.from(accepts.keys()))
     } else {
       this.fixedDirection = false
     }
@@ -59,13 +59,13 @@ export class Player {
     const callback = ({
       [Tile.Spawner]: async () => {
         console.log(`獲得 50 分`)
-        state.messages.push(`第 ${this.id} 組 獲得 50 分`)
+        state.messages.push(`玩家 ${this.id} 獲得 50 分`)
         this.score += 50
 
         const victoryScore = state.victoryScore
         if (victoryScore <= this.score) {
           console.log(`獲勝`)
-          state.messages.push(`第 ${this.id} 組 獲勝`)
+          state.messages.push(`玩家 ${this.id} 獲勝`)
           state.winner = this
           useGame().running = false
         }
@@ -74,28 +74,28 @@ export class Player {
       [Tile.Task]: async () => {
         const task = randomTask()
         console.log(`獲得任務 ${task}`)
-        state.messages.push(`第 ${this.id} 組 獲得任務 ${task[0]}`)
+        state.messages.push(`玩家 ${this.id} 獲得任務 ${task[0]}`)
         await state.setEvent(this, "任務", task, true)
       },
 
       [Tile.Opportunity]: async () => {
         const opportunity = randomOpportunity()
         console.log(`獲得機會 ${opportunity}`)
-        state.messages.push(`第 ${this.id} 組 獲得機會 ${opportunity[0]}`)
+        state.messages.push(`玩家 ${this.id} 獲得機會 ${opportunity[0]}`)
         await state.setEvent(this, "機會", opportunity)
       },
 
       [Tile.Destiny]: async () => {
         const destiny = randomDestiny()
         console.log(`獲得命運 ${destiny}`)
-        state.messages.push(`第 ${this.id} 組 獲得命運 ${destiny[0]}`)
+        state.messages.push(`玩家 ${this.id} 獲得命運 ${destiny[0]}`)
         await state.setEvent(this, "命運", destiny)
       },
 
       [Tile.Punishment]: async () => {
         const punishment = randomPunishment()
         console.log(`獲得懲罰 ${punishment}`)
-        state.messages.push(`第 ${this.id} 組 獲得懲罰 ${punishment[0]}`)
+        state.messages.push(`玩家 ${this.id} 獲得懲罰 ${punishment[0]}`)
         await state.setEvent(this, "懲罰", punishment)
       },
 
@@ -105,30 +105,31 @@ export class Player {
 
         const teleports = map.teleports.filter(teleport => teleport[0] !== this.position.x && teleport[1] !== this.position.y)
         shuffle(teleports)
-        
+
         const teleport = teleports.pop()!
         this.position.set(teleport[0], teleport[1], 200)
         console.log(`傳送至 ${teleport}`)
-        state.messages.push(`第 ${this.id} 組 傳送至 ${teleport}`)
-        
+        state.messages.push(`玩家 ${this.id} 傳送至 ${teleport}`)
+
         const directions = Array.from(map.nextPosition([teleport[0], teleport[1]]).keys())
         shuffle(directions)
-        
+
         this.direction = directions.pop()!
         this.fixedDirection = true
-
+        
         useGameRender().position = this.position.clone()
+        await inputs.wait(500)
       },
 
       [Tile.Prison]: async () => {
         console.log(`被關 1 回合`)
-        state.messages.push(`第 ${this.id} 組 被關 1 回合`)
+        state.messages.push(`玩家 ${this.id} 被關 1 回合`)
         this.dizziness++
       },
 
       [Tile.Hospital]: async () => {
         console.log(`被治療 1 回合`)
-        state.messages.push(`第 ${this.id} 組 被治療 1 回合`)
+        state.messages.push(`玩家 ${this.id} 被治療 1 回合`)
         this.health = DEFAULT_PLAYER_HEALTH
         this.dizziness++
       },
@@ -147,7 +148,7 @@ export class Player {
 
     if (map.getTile(this.position.x, this.position.y) === Tile.Hospital) {
       console.log(`玩家 ${this.id} 在醫院，無法攻擊`)
-      state.messages.push(`第 ${this.id} 組 在醫院，無法攻擊`)
+      state.messages.push(`玩家 ${this.id} 在醫院，無法攻擊`)
       return
     }
 
@@ -157,7 +158,7 @@ export class Player {
     const inputs = useGameInputs();
     await inputs.next.input("進行攻擊")
     state.steps = 0
-    
+
     other.health -= plunder
     console.log(`造成 ${plunder} 點傷害，剩餘 ${other.health} 點血量`)
 
