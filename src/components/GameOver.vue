f8153e4125933e95b2fb951985fc1340ac2b8a2b
xiao-e-yun <a0970323059@gmail.com>
2024-12-02 02:08:09 +0800

    修改結局布局

--- src/components/GameOver.vue
+++ src/components/GameOver.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { useGameState } from '../game/state';
-import GameRanking from './game/Ranking.vue';
 
 const state = useGameState()
+const players = computed(() => Array.from(state.players.values()))
 </script>
 
 <template>
   <h1>遊戲結束</h1>
-  最終贏家: 第 {{ state.winner }} 組
-  <GameRanking />
+  最終贏家是... 第 {{ state.winner!.id }} 組！
+  
+  <div class="player" v-for="player in players.toSorted((a, b) => b.score - a.score)">
+    <h3>第 {{ player.id }} 組  <i>{{player === state.winner ? '👑' : '👻'}}</i></h3>
+    <span>分數: {{ player.score }}</span>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.player {
+  margin: 0.5rem auto;
+  padding: 0.2rem 0.5rem;
+  border-radius: 0.5rem;
+  background: #555;
+  width: 16em;
+  min-width: 20vw;
+
+  h3 {
+    margin: 0;
+    font-size: 1.2rem;
+  }
+}
 </style>
